0be4b113f66422b4abea338ad483b82185cd8bd5
Sumnima123 <sumnimasharma6@gmail.com>
2021-01-09 23:18:07 -0600

    Product images added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
 import products from '../products'
 
 
@@ -13,6 +14,8 @@ const HomeScreen = () => {
             {products.map(product => (
                 <Col sm={12} md={6} lg={4} xl={3}> 
                 <h3>{product.name} </h3>
+
+                <Product product={product}/>
                 </Col>
             ))}    
         </Row>
